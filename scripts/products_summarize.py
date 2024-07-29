ab3e286c9a80377bc3c6b4da5433dea2a340d05f
Albert Sund Aillet <albert@sundaillet.com>
2024-07-29 17:51:59 +0200

    added nurtient source in products summary and cleaned up script

--- scripts/products_summarize.py
+++ scripts/products_summarize.py
@@ -12,86 +12,53 @@ from typing import Any
 
 DATA_DIR = Path(os.getenv("DATA_DIR", ""))
 
-nutrients = [
-    "alcohol_100g",
-    "alcohol_unit",
-    "calcium_100g",
-    "calcium_unit",
-    "carbohydrates_100g",
-    "carbohydrates_unit",
-    # "energy",
-    # "energy-kcal",
-    "energy-kcal_100g",
-    "energy-kcal_unit",
-    # "energy-kcal_value",
-    # "energy-kcal_value_computed",
-    # "energy_100g",
-    # "energy_unit",
-    # "energy_value",
-    "fat_100g",
-    "fat_unit",
-    "fiber_100g",
-    "fiber_unit",
-    "proteins_100g",
-    "proteins_unit",
-    "salt_100g",
-    "salt_unit",
-    "saturated-fat_100g",
-    "saturated-fat_unit",
-    "sodium_100g",
-    "sodium_unit",
-    "sugars_100g",
-    "sugars_unit",
-    "vitamin-b9_100g",
-    "vitamin-b9_unit",
-]
 
 all_estimated_nutrients = [
-    "alcohol_100g",
-    "beta-carotene_100g",
-    "calcium_100g",
-    "carbohydrates_100g",
-    "cholesterol_100g",
-    "copper_100g",
-    "energy-kcal_100g",
-    "energy-kj_100g",
-    "energy_100g",
-    "fat_100g",
-    "fiber_100g",
-    "fructose_100g",
-    "galactose_100g",
-    "glucose_100g",
-    "iodine_100g",
-    "iron_100g",
-    "lactose_100g",
-    "magnesium_100g",
-    "maltose_100g",
-    "manganese_100g",
-    "pantothenic-acid_100g",
-    "phosphorus_100g",
-    "phylloquinone_100g",
-    "polyols_100g",
-    "potassium_100g",
-    "proteins_100g",
-    "salt_100g",
-    "saturated-fat_100g",
-    "selenium_100g",
-    "sodium_100g",
-    "starch_100g",
-    "sucrose_100g",
-    "sugars_100g",
-    "vitamin-a_100g",
-    "vitamin-b12_100g",
-    "vitamin-b1_100g",
-    "vitamin-b2_100g",
-    "vitamin-b6_100g",
-    "vitamin-b9_100g",
-    "vitamin-c_100g",
-    "vitamin-d_100g",
-    "vitamin-e_100g",
-    "vitamin-pp_100g",
-    "water_100g",
-    "zinc_100g",
+    "alcohol",
+    "beta-carotene",
+    "calcium",
+    "carbohydrates",
+    "cholesterol",
+    "copper",
+    "energy-kcal",
+    "energy-kj",
+    "energy",
+    "fat",
+    "fiber",
+    "fructose",
+    "galactose",
+    "glucose",
+    "iodine",
+    "iron",
+    "lactose",
+    "magnesium",
+    "maltose",
+    "manganese",
+    "pantothenic-acid",
+    "phosphorus",
+    "phylloquinone",
+    "polyols",
+    "potassium",
+    "proteins",
+    "salt",
+    "saturated-fat",
+    "selenium",
+    "sodium",
+    "starch",
+    "sucrose",
+    "sugars",
+    "vitamin-a",
+    "vitamin-b12",
+    "vitamin-b1",
+    "vitamin-b2",
+    "vitamin-b6",
+    "vitamin-b9",
+    "vitamin-c",
+    "vitamin-d",
+    "vitamin-e",
+    "vitamin-pp",
+    "water",
+    "zinc",
 ]
 
 # For getting it at the moment of looking at them replace main with the permalink:
@@ -154,7 +121,7 @@ def adapt_ciqual_column_name(name: str) -> str | None:
         if " or " in new_name:
             new_name = new_name.split(" or ")[1]
         new_name = new_name.replace(" ", "-")
-    assert new_name + "_100g" in all_estimated_nutrients, (name, new_name)
+    assert new_name in all_estimated_nutrients, (name, new_name)
     return new_name
 
 
@@ -194,8 +161,7 @@ def load_ciqual_database(file):
 
 
 def create_csv(file, items: list[dict[str, Any]], ciqual_lookup: dict[str, dict[str, Any]]):
-    nutrient_units = [name.replace("_100g", "_unit") for name in all_estimated_nutrients]
-    nutrient_header = [item for tup in zip(all_estimated_nutrients, nutrient_units, strict=True) for item in tup]
+    nutrient_header = [name + suffix for name in all_estimated_nutrients for suffix in ("_100g", "_unit", "_source")]
     header = ["product_code", "product_name", "ciqual_code", *nutrient_header]
 
     writer = csv.writer(file)
@@ -208,7 +174,7 @@ def create_csv(file, items: list[dict[str, Any]], ciqual_lookup: dict[str, dict[
         # Check that all estimated nutrients seem to have the same keys
         estimated_nutrients = item["product"].get("nutriments_estimated")
         if estimated_nutrients is not None:
-            assert set(estimated_nutrients.keys()) == set(all_estimated_nutrients)
+            assert set(estimated_nutrients.keys()) == set(name + "_100g" for name in all_estimated_nutrients)
             # NOTE: The OFF estimated nutrients are not used as the unit have not been checked.
 
         ciqual_code = item["product"]["categories_properties"].get("ciqual_food_code:en")
@@ -216,13 +182,17 @@ def create_csv(file, items: list[dict[str, Any]], ciqual_lookup: dict[str, dict[
 
         nutrients = {}
         for nurtient_name in all_estimated_nutrients:
-            nurtient_unit = nurtient_name.replace("_100g", "_unit")
-            if nurtient_name in reported_nutrients:
-                nutrients[nurtient_name] = reported_nutrients[nurtient_name]
+            nurtient_100g = nurtient_name + "_100g"
+            nurtient_unit = nurtient_name + "_unit"
+            nutrient_source = nurtient_name + "_source"
+            if nurtient_100g in reported_nutrients:
+                nutrients[nurtient_100g] = reported_nutrients[nurtient_100g]
                 nutrients[nurtient_unit] = reported_nutrients[nurtient_unit]
-            elif nurtient_name in ciqual_nutrients:
-                nutrients[nurtient_name] = ciqual_nutrients[nurtient_name]
+                nutrients[nutrient_source] = "reported"
+            elif nurtient_100g in ciqual_nutrients:
+                nutrients[nurtient_100g] = ciqual_nutrients[nurtient_100g]
                 nutrients[nurtient_unit] = ciqual_nutrients[nurtient_unit]
+                nutrients[nutrient_source] = "ciqual"
 
         row_dict = {
             "product_name": item["product"].get("product_name"),
