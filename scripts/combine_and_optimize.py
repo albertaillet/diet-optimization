00d455e326338481e349ba83f5c6ea9d619b6875
Albert Sund Aillet <albert@sundaillet.com>
2024-07-30 18:16:47 +0200

    added function to get upper and lower bounds from recommendations csv

--- scripts/combine_and_optimize.py
+++ scripts/combine_and_optimize.py
@@ -12,7 +12,7 @@ from scipy.optimize import linprog
 
 DATA_DIR = Path("/home/alsundai/git/diet-optimization/data")
 
-all_estimated_nutrients = [
+used_nutrients = [
     # "alcohol",
     # "beta-carotene",
     "calcium",
@@ -59,25 +59,18 @@ all_estimated_nutrients = [
     # "water",
     "zinc",
 ]
-pd.set_option("display.max_rows", None)
-pd.set_option("display.max_columns", None)
-
-
-prices = pd.read_csv(DATA_DIR / "prices.csv")
-products = pd.read_csv(DATA_DIR / "products.csv")
-recommendations = pd.read_csv(DATA_DIR / "recommendations.csv")
 
 
 def filter_products(products: pd.DataFrame) -> pd.DataFrame:
     """Filters to only the relevant nutrients and drops all products with missing values."""
     cols = ["product_code", "product_name", "ciqual_code"]
-    nutrient_cols = [name + suffix for name in all_estimated_nutrients for suffix in ("_100g", "_unit", "_source")]
+    nutrient_cols = [name + suffix for name in used_nutrients for suffix in ("_100g", "_unit", "_source")]
     relevant_products = products[cols + nutrient_cols].dropna()
     # NOTE: temporary check
     assert relevant_products.shape == (26, len(nutrient_cols) + 3), (relevant_products.shape, (25, len(nutrient_cols) + 3))
 
     # Check that all columns have the same unit and source between rows.
-    for nutient in all_estimated_nutrients:
+    for nutient in used_nutrients:
         unique_units = relevant_products[nutient + "_unit"].unique()
         assert len(unique_units) == 1, (nutient, unique_units)
         if nutient not in ("fiber", "calcium"):
@@ -99,67 +92,73 @@ def fix_prices(prices: pd.DataFrame) -> pd.DataFrame:
     return latest_prices.rename(columns={"price": "price_chf"}).drop(columns=["currency"])
 
 
+def get_recommendations_upper_and_lower_bounds(
+    recommendations: pd.DataFrame, products_and_prices: pd.DataFrame
+) -> tuple[np.ndarray, np.ndarray]:
+    indexed_recommendations = recommendations.set_index("nutrient")
+
+    # Adding macronutrients to the micronutrient dataframe.
+    # NOTE: same for males and females
+    additional_recommendations_lb_ub_unit = {
+        "carbohydrates": (0, 200, "g"),
+        "fiber": (60, np.nan, "g"),
+        "energy-kcal": (1_500, 3_000, "kcal"),
+        "fat": (60, np.nan, "g"),
+        "saturated-fat": (0, np.nan, "g"),
+        "proteins": (120, np.nan, "g"),
+    }
+    for nutrient, (lb, ub, unit) in additional_recommendations_lb_ub_unit.items():
+        indexed_recommendations.loc[nutrient] = [unit, None, lb, lb, ub]
+
+    # Check that the upper and lower bounds nutrients use the same units as the product nutrients.
+    for nutrient in used_nutrients:
+        product_unique_units = set(products_and_prices[nutrient + "_unit"].unique())
+        recommendation_unit = indexed_recommendations.loc[nutrient, "unit"]
+        assert product_unique_units == {recommendation_unit}, (nutrient, product_unique_units, recommendation_unit)
+
+    # Lower bounds for nutrients
+    value_key = "value_males"  # "value_females"  # NOTE: using male values
+    lb = indexed_recommendations[value_key][used_nutrients].values
+
+    # Upper bounds for nutrients
+    ub = indexed_recommendations["value_upper_intake"][used_nutrients].values
+
+    # lb.shape, ub.shape  # (n_nutrients,)
+    return lb, ub
+
+
+def solve_optimization(A, lb, ub, c):
+    # Constraints for lower bounds
+    A_ub_lb = -A.T
+    b_ub_lb = -lb
+
+    # Constraints for upper bounds
+    A_ub_ub = A.T[~np.isnan(ub)]
+    b_ub_ub = ub[~np.isnan(ub)]
+
+    # Concatenate both constraints
+    A_ub = np.vstack([A_ub_lb, A_ub_ub])
+    b_ub = np.concatenate([b_ub_lb, b_ub_ub])
+
+    # Solve the problem and result the result.
+    return linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=(0, None))
+
+
+prices = pd.read_csv(DATA_DIR / "prices.csv")
+products = pd.read_csv(DATA_DIR / "products.csv")
+recommendations = pd.read_csv(DATA_DIR / "recommendations.csv")
+
 relevant_products = filter_products(products)
 fixed_prices = fix_prices(prices)
-
-# %%
 products_and_prices = pd.merge(relevant_products, fixed_prices, how="inner", on=["product_code", "product_name"])
-products_and_prices.head(40)
 
-# %%
-A_nutrients = products_and_prices[[n + "_100g" for n in all_estimated_nutrients]].values
-# A_nutrients.shape  # (n_products, n_nutrients)
+A_nutrients = products_and_prices[[n + "_100g" for n in used_nutrients]].values
 
-# %%
 c_costs = 0.1 * products_and_prices["price_chf"].values.astype("float")  # to price per kg to price per 100g
-# c_costs.shape  # (n_products,)
 
-# %%
-# TODO: hardcoded at the moment for testing.
-recommendations_lb_ub_unit = {
-    "carbohydrates": (0, 200, "g"),
-    "calcium": (950, 2500, "mg"),
-    "copper": (0.9, 5, "mg"),  # NOTE: setting a higher lower bound for copper makes the problem unfeasible
-    "fiber": (60, np.nan, "g"),
-    "energy-kcal": (1_500, 3_000, "kcal"),
-    "fat": (60, np.nan, "g"),
-    "potassium": (3500, np.nan, "mg"),
-    "magnesium": (350, np.nan, "mg"),
-    "saturated-fat": (0, np.nan, "g"),
-    "selenium": (90, 255, "µg"),  # noqa: RUF001
-    "iron": (9, 60, "mg"),
-    "proteins": (120, np.nan, "g"),
-    "vitamin-b12": (4, np.nan, "µg"),  # noqa: RUF001
-    "vitamin-b6": (1.8, 12, "mg"),
-    "vitamin-c": (110, np.nan, "mg"),
-    "vitamin-e": (11, 200, "mg"),
-    "zinc": (12.7, 25, "mg"),
-}
-for nutrient in all_estimated_nutrients:
-    unique_units = set(products_and_prices[nutrient + "_unit"].unique())
-    recommendation_unit = recommendations_lb_ub_unit[nutrient][2]
-    assert unique_units == {recommendation_unit}, (nutrient, unique_units, recommendation_unit)
+lb, ub = get_recommendations_upper_and_lower_bounds(recommendations, products_and_prices)
 
-# do this and print result using scipy.optimize.linprog
-# Lower bounds for nutrients
-lb = np.array([recommendations_lb_ub_unit[nutrient][0] for nutrient in all_estimated_nutrients])
-ub = np.array([recommendations_lb_ub_unit[nutrient][1] for nutrient in all_estimated_nutrients])
-# lb.shape, ub.shape  # (n_nutrients,)
-
-# Constraints for lower bounds
-A_ub_lb = -A_nutrients.T
-b_ub_lb = -lb
-
-# Constraints for upper bounds
-A_ub_ub = A_nutrients.T[~np.isnan(ub)]
-b_ub_ub = ub[~np.isnan(ub)]
-
-# Concatenate both constraints
-A_ub = np.vstack([A_ub_lb, A_ub_ub])
-b_ub = np.concatenate([b_ub_lb, b_ub_ub])
-
-result = linprog(c_costs, A_ub=A_ub, b_ub=b_ub, bounds=(0, None))
-
+result = solve_optimization(A_nutrients, lb, ub, c_costs)
 result_table = pd.DataFrame({
     # "product_code": products_and_prices["product_code"],
     "product_name": products_and_prices["product_name"],
@@ -169,8 +168,9 @@ result_table = pd.DataFrame({
 print(result_table[result_table["quantity_g"] > 0].head(30))
 
 nutrient_results = A_nutrients.T @ result.x
-nutrient_result_table = pd.DataFrame({"nutrient": all_estimated_nutrients, "value": nutrient_results})
+nutrient_result_table = pd.DataFrame({"nutrient": used_nutrients, "value": nutrient_results})
 print(nutrient_result_table)
 
 print("Price per day:", round(result.fun, 4), "CHF")
+
 # %%
