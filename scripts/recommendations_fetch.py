7a25251a398f3744f4a9a76fca6825fd989b8e10
Albert Sund Aillet <albert@sundaillet.com>
2024-08-06 18:24:47 +0200

    added web.archive.org snapshot to recommendations_fetch.py

--- scripts/recommendations_fetch.py
+++ scripts/recommendations_fetch.py
@@ -11,6 +11,7 @@ import requests
 DATA_DIR = Path(os.getenv("DATA_DIR", ""))
 
 URL = "https://pub.norden.org/nord2023-003/recommendations.html"
+# Snapshot 29 July 2024: https://web.archive.org/web/20240729193940/https://pub.norden.org/nord2023-003/recommendations.html
 
 
 if __name__ == "__main__":
