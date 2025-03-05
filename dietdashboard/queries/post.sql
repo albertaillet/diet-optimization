2b5f8f148e589e7ab6d5a9fdf14c0ee29d6b3716
Albert Sund Aillet <albert@sundaillet.com>
2025-03-05 19:06:05 +0100

    added sql to drop all intermediate tables

---
+++ dietdashboard/queries/post.sql
@@ -0,0 +1,13 @@
+-- Drop all intermediate tables
+DROP TABLE nutrient_map;
+DROP TABLE ciqual_alim;
+DROP TABLE ciqual_compo;
+DROP TABLE ciqual_sources;
+DROP TABLE calnut_0;
+DROP TABLE calnut_1;
+DROP TABLE prices;
+DROP TABLE products;
+DROP TABLE products_with_ciqual_and_price;
+DROP TABLE products_nutriments;
+DROP TABLE products_nutriments_selected;
+DROP TABLE final_nutrient_table;
