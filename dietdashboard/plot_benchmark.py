282a646f0a70b3ba339490fa0d875a08aa27786f
Albert Sund Aillet <albert@sundaillet.com>
2025-03-11 23:09:37 +0100

    added colors to plot

--- dietdashboard/plot_benchmark.py
+++ dietdashboard/plot_benchmark.py
@@ -2,6 +2,8 @@
 import json
 from pathlib import Path
 
+import matplotlib.cm as cm
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 
 DATA_DIR = Path(__file__).parent.parent / "data"
@@ -18,10 +20,24 @@ with BENCHMARK_RESULTS.open() as f:
 # %%
 limits = [int(n) for n in resluts]
 methods = list(resluts[limits[0]].keys())
+
+# Sort methods by the sum of times across all limits
+method_times_sum = {method: sum(resluts[n][method][0] for n in limits) for method in methods}
+sorted_methods = sorted(methods, key=lambda m: method_times_sum[m], reverse=True)
+
+# Set up a colormap
+
+# Normalize times for colormap
+method_times_array = [method_times_sum[m] for m in sorted_methods]
+norm = colors.LogNorm(vmin=min(method_times_array), vmax=max(method_times_array))
+cmap = cm.tab20b  # type: ignore
+
 fig, ax = plt.subplots(figsize=(10, 6))
-for method in methods:
+for method in sorted_methods:
     times = [resluts[n][method][0] for n in limits]
-    ax.plot(limits, times, label=method, marker="x", markersize=4)
+    color = cmap(norm(method_times_sum[method]))
+    ax.plot(limits, times, label=f"{method} (Σ={method_times_sum[method]:.2f}s)", marker="x", markersize=4, color=color)
+
 ax.set_xlabel("Number of rows", fontsize=8)
 ax.set_ylabel("Time (s)", fontsize=8)
 ax.legend(fontsize=7)
@@ -29,6 +45,14 @@ ax.tick_params(axis="both", which="major", labelsize=7)
 ax.set_yscale("log")
 ax.set_xscale("log")
 ax.grid()
+
+# Add colorbar
+sm = cm.ScalarMappable(cmap=cmap, norm=norm)
+sm.set_array([])
+cbar = plt.colorbar(sm, ax=ax)
+cbar.set_label("Total time (s)", fontsize=8)
+cbar.ax.tick_params(labelsize=7)
+
 plt.tight_layout()
 
 # %%
