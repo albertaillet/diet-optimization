7fa794888fe9d23e41855250bc40a6c6bda94305
Albert Sund Aillet <albert@sundaillet.com>
2025-03-13 23:05:05 +0100

    tweaks to plot benchmark

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,5 +1,6 @@
 # %%
 import csv
+import operator
 from pathlib import Path
 
 import matplotlib.cm as cm
@@ -21,22 +22,20 @@ solvers = set(row["solver_id"] for row in resluts)
 assert all(any(row["size"] == "500" for row in resluts if row["solver_id"] == s) for s in solvers)
 
 # Sort methods by the min of times for size 500
-method_times_500 = {
-    m: min(float(row["time"]) for row in resluts if row["solver_id"] == m and row["size"] == "500") for m in solvers
-}
-sorted_methods = sorted(solvers, key=lambda m: method_times_500[m], reverse=True)
+m_rows = [next(row for row in resluts if row["solver_id"] == m and row["size"] == "500") for m in solvers]
+method_and_time = [(row["solver_id"], float(row["time"])) for row in m_rows]
+method_and_time = sorted(method_and_time, key=operator.itemgetter(1), reverse=True)
 
 # Normalize times for colormap
-method_times_array = [method_times_500[m] for m in sorted_methods]
-norm = colors.LogNorm(vmin=min(method_times_array), vmax=max(method_times_array))
+norm = colors.LogNorm(vmin=min(t for _, t in method_and_time), vmax=max(t for _, t in method_and_time))
 cmap = cm.tab20b  # type: ignore
 
 fig, ax = plt.subplots(figsize=(10, 6))
-for method in sorted_methods:
+for method, method_time in method_and_time:
     xy = [(float(row["size"]), float(row["time"])) for row in resluts if row["solver_id"] == method]
     sizes, times = zip(*sorted(xy), strict=True)
-    color = cmap(norm(method_times_500[method]))  # Use the same color for all points of the same method
-    ax.plot(sizes, times, label=f"{method} ({1000 * method_times_500[method]:.2f}ms)", marker="x", markersize=4, color=color)
+    color = cmap(norm(method_time))  # Use the same color for all points of the same method
+    ax.plot(sizes, times, label=f"{method} ({1000 * method_time:.2f}ms)", marker="x", markersize=4, color=color)
 
 ax.set_xlabel("Number of rows", fontsize=8)
 ax.set_ylabel("Time (s)", fontsize=8)
