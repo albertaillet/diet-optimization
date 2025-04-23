c4b9d66000d0e7c1e2bbf1d81f0c06ff3dc04075
Albert Sund Aillet <albert@sundaillet.com>
2025-04-23 20:22:42 +0200

    added macro table

--- dietdashboard/frontend/js/result.js
+++ dietdashboard/frontend/js/result.js
@@ -17,14 +17,38 @@ function displayResultTable(container, data) {
     .html(d => `<a href="${d.link}" target="_blank">${d.text}</a>`);
 }
 
+function displayResultMacroTable(container, data) {
+  const fields = ["protein", "carbohydrate", "fat"];
+  if (!(data.length && fields.every(field => field in data[0]))) {
+    d3.select(container).selectAll("tr").remove(); // TODO: Handle this better
+    return;
+  }
+  const weight = Object.fromEntries(fields.map(key => [key, d3.sum(data, d => +d[key] || 0)]));
+  const energy = { protein: weight.protein * 4, carbohydrate: weight.carbohydrate * 4, fat: weight.fat * 9 };
+  const totalEnergy = d3.sum(Object.values(energy));
+  d3.select(container)
+    .selectAll("tr")
+    .data([
+      ["Weight", ...fields.map(field => `${weight[field].toFixed(0)}g`)],
+      ["Energy", ...fields.map(field => `${energy[field].toFixed(0)} kcal`)],
+      ["Percentage", ...fields.map(field => `${((energy[field] / totalEnergy) * 100).toFixed(0)}%`)]
+    ])
+    .join("tr")
+    .selectAll("td")
+    .data(d => d)
+    .join("td")
+    .html(d => d);
+  // TODO: Pie chart
+}
+
 const currencySelector = document.getElementById("currency");
 const resultTable = document.getElementById("result-table");
-const resultSummary = d3.select("#result-summary");
+const resultMacroTable = document.getElementById("macro-table-body");
 
 export function updateResult(products) {
   const totalPrice = d3.sum(products, d => +d.price || 0).toFixed(2);
   document.getElementById("result-price").innerHTML = `${totalPrice} ${currencySelector.value}`;
 
   displayResultTable(resultTable, products);
-  // TODO: displayResultSummary(resultSummary, products);
+  displayResultMacroTable(resultMacroTable, products);
 }
