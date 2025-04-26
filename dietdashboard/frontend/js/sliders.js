a3c5b0e0fc4d3787fb83170ff3bfc341f16a4950
Albert Sund Aillet <albert@sundaillet.com>
2025-04-26 11:17:30 +0200

    update content with join instead of full rerender

--- dietdashboard/frontend/js/sliders.js
+++ dietdashboard/frontend/js/sliders.js
@@ -4,7 +4,7 @@ import { optimize } from "./index";
 
 // Common configuration values
 const CONFIG = {
-  margin: { top: 5, right: 10, bottom: 20, left: 10 },
+  margin: { top: 5, right: 20, bottom: 20, left: 10 },
   svgHeight: 50,
   nTicks: 10,
   barHeight: 10,
@@ -18,7 +18,8 @@ const CONFIG = {
  */
 function openModal(event, d) {
   form.addEventListener("submit", event => {
-    if (event.submitter.name != "save") return;
+    const btn = event.submitter || document.activeElement;
+    if (btn.name !== "save") return;
     const formData = new FormData(form);
     const newMin = Number(formData.get("minVal"));
     const newMax = Number(formData.get("maxVal"));
@@ -46,12 +47,14 @@ function openModal(event, d) {
  * @param {string} nutrientId
  */
 function createSegmentsData(products, nutrientId) {
-  const segments = products.map((p, i) => ({
-    i: i,
-    id: p.id,
-    name: p.product_name,
-    level: Number(p[nutrientId]) || 0
-  }));
+  const segments = products
+    .map((p, i) => ({
+      i: i,
+      id: p.id,
+      name: p.product_name,
+      level: Number(p[nutrientId]) || 0
+    }))
+    .reverse();
   let cum = 0;
   segments.forEach(p => {
     p.startValue = cum;
@@ -62,53 +65,64 @@ function createSegmentsData(products, nutrientId) {
 }
 
 /**
- * @param {d3.Selection} barGroup
+ * @param {d3.Selection} slider
  * @param {Array} segments
  * @param {d3.Scale} x
  */
-function Segment(barGroup, segments, x) {
+function Segment(slider, segments, x) {
   // Setup bar positioning
   const height = CONFIG.svgHeight - CONFIG.margin.top - CONFIG.margin.bottom;
   const axisYPosition = height;
   const barYPosition = axisYPosition - CONFIG.barHeight - 2;
   const barHeight = CONFIG.barHeight; // From config
-  const segmentGroups = barGroup
+  const labelYPosition = barYPosition - 5;
+  slider
     .selectAll("g.segment-group")
-    .data(segments, p => `${p.id}-${p.quantity_g}`)
-    .join("g")
-    .attr("class", "segment-group");
-
-  // Append rectangles to entering segments (start with 0 width at final position)
-  segmentGroups
-    .append("rect")
-    .attr("class", "segment")
-    .attr("y", barYPosition)
-    .attr("height", barHeight)
-    .attr("x", 0)
-    .attr("width", 0)
-    .attr("fill", p => d3.schemeTableau10[p.i % 10])
-    .transition()
-    .duration(750)
-    .attr("width", p => x(p.endValue) - x(p.startValue))
-    .attr("x", p => x(p.startValue));
-
-  segmentGroups
-    .append("text")
-    .attr("class", "segment-label")
-    .attr("y", barYPosition - 5) // Position above bar
-    .attr("x", d => x((d.startValue + d.endValue) / 2)) // Center based on final values
-    .text(d => d.name);
+    .data(segments, d => d.i)
+    .join(
+      enter =>
+        enter
+          .append("g")
+          .attr("class", "segment-group")
+          .call(segmentGroup => segmentGroup.append("text").attr("class", "segment-label").attr("y", labelYPosition))
+          .call(
+            segmentGroup =>
+              segmentGroup
+                .append("rect")
+                .attr("class", "segment")
+                .attr("y", barYPosition)
+                .attr("height", barHeight)
+                .attr("x", 0)
+                .attr("width", 0)
+                .attr("fill", p => d3.schemeTableau10[p.i % 10]) // TODO: define this in the data.
+          ),
+      update => update,
+      exit => exit.transition().duration(100).style("opacity", 0).remove()
+    )
+    .call(segmentGroup =>
+      segmentGroup
+        .select("text.segment-label")
+        .attr("x", d => x((d.startValue + d.endValue) / 2))
+        .text(d => d.name)
+    )
+    .call(segmentGroup =>
+      segmentGroup
+        .select("rect.segment")
+        .transition()
+        .duration(750)
+        .attr("x", p => x(p.startValue))
+        .attr("width", p => x(p.endValue) - x(p.startValue))
+    );
 }
 
 /**
- * @param {d3.Selection} g
+ * @param {d3.Selection} slider
  * @param {object} d
  * @param {d3.Scale} x
  * @param {number} height
  * @param {number} width
  */
-function Brush(g, d, x, height, width) {
-  const { min, max, lower, upper, id } = d; // Get state from the passed object
+function Brush(slider, d, x, height, width) {
   const brushSelection = d3
     .brushX()
     .extent([
@@ -125,18 +139,27 @@ function Brush(g, d, x, height, width) {
       optimize();
     });
 
-  const brushGroup = g.append("g").attr("class", "brush").call(brushSelection);
+  const brushGroup = slider
+    .selectAll("g.brush")
+    .data([d], d => d.id)
+    .join(
+      enter => enter.append("g").attr("class", "brush"),
+      update => update,
+      exit => exit.remove()
+    )
+    .call(brushSelection)
+    .raise(); // Raise the brush above the segments
 
   const handle = brushGroup
-    .selectAll(".brush-handle")
+    .selectAll("g.brush-handle")
     .data([{ type: "w" }, { type: "e" }])
-    .enter()
-    .append("g")
-    .attr("class", "brush-handle")
-    .attr("cursor", "ew-resize");
-
-  handle.append("circle").attr("r", CONFIG.handleRadius).attr("cy", height);
-  brushGroup.call(brushSelection.move, [lower, upper].map(x));
+    .join(enter =>
+      enter
+        .append("g")
+        .attr("class", "brush-handle")
+        .call(g => g.append("circle").attr("r", CONFIG.handleRadius).attr("cy", height))
+    );
+  brushGroup.call(brushSelection.move, [d.lower, d.upper].map(x));
 }
 
 const modal = document.getElementById("rangeModal");
@@ -147,7 +170,7 @@ if (!modal || !form || !modalTitle) {
 }
 
 export function Sliders(productsData, sliderData) {
-  const data = sliderData.filter(d => d.active == 1);
+  const data = sliderData.filter(d => d.active);
   data.forEach(d => (d.segments = createSegmentsData(productsData, d.id)));
   const height = CONFIG.svgHeight - CONFIG.margin.top - CONFIG.margin.bottom;
   // Local variables for each SVG element
@@ -171,27 +194,32 @@ export function Sliders(productsData, sliderData) {
           .append("td")
           .append("svg")
           .attr("width", "100%")
-          .attr("height", CONFIG.svgHeight),
-      update => update.select("svg"),
+          .attr("height", CONFIG.svgHeight)
+          .append("g")
+          .attr("class", "slider"),
+      update => update.select("svg").select("g.slider"),
       exit => exit.remove()
     )
     .each(function (d) {
       // Setup local variables for each SVG element
-      d3.select(this).selectAll("*").remove(); // Clear previous content TODO: update the content in a more efficient way
-      const w = this.clientWidth - CONFIG.margin.left - CONFIG.margin.right;
+      const w = this.parentNode.clientWidth - CONFIG.margin.left - CONFIG.margin.right;
       x.set(this, d3.scaleLinear().domain([d.min, d.max]).range([0, w]));
       width.set(this, w);
     })
-    .append("g")
     .attr("transform", `translate(${CONFIG.margin.left},${CONFIG.margin.top})`)
     .each(function (d) {
-      Segment(d3.select(this), d.segments, x.get(this));
+      d3.select(this)
+        .selectAll("g.axis")
+        .data([d])
+        .join(
+          enter => enter.append("g").attr("class", "axis").attr("transform", `translate(0,${height})`),
+          update => update,
+          exit => exit.remove()
+        )
+        .call(d3.axisBottom(x.get(this)).ticks(CONFIG.nTicks).tickSize(6).tickPadding(3));
     })
     .each(function (d) {
-      d3.select(this)
-        .append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x.get(this)).ticks(CONFIG.nTicks).tickSize(6).tickPadding(3));
+      Segment(d3.select(this), d.segments, x.get(this));
     })
     .each(function (d) {
       Brush(d3.select(this), d, x.get(this), height, width.get(this));
