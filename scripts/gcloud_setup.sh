b184128eb38244cd73889c738392c602044bd703
Albert Sund Aillet <albert@sundaillet.com>
2025-08-30 16:55:18 +0200

    added further gcloud setup

--- scripts/gcloud_setup.sh
+++ scripts/gcloud_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export PROJECT_ID="diet-optimization-460319"
 export GITHUB_ORG="albertaillet"
+export GITHUB_REPO="diet-optimization"
 
 echo "You probably should run this step by step by pasting each command into your terminal."
 exit
@@ -31,9 +32,6 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
   --member="serviceAccount:runtime-sa@${PROJECT_ID}.iam.gserviceaccount.com" \
   --role="roles/run.invoker"
 
-# Get your project number
-gcloud projects describe "${PROJECT_ID}" --format="value(projectNumber)"
-
 # Documentation for Workload Identity Federation
 # https://github.com/google-github-actions/auth/blob/0dfce0c0f81ee698e8ca7d23b8a0b0706f6370e3/README.md#preferred-direct-workload-identity-federation
 
@@ -44,10 +42,7 @@ gcloud iam workload-identity-pools create github-actions-pool \
   --project="${PROJECT_ID}"
 
 # Get the Workload Identity Pool ID
-gcloud iam workload-identity-pools describe github-actions-pool \
-  --project="${PROJECT_ID}" \
-  --location="global" \
-  --format="value(name)"
+gcloud iam workload-identity-pools describe github-actions-pool --project="${PROJECT_ID}" --location="global" --format="value(name)"
 
 # Create a Workload Identity Provider within the pool for GitHub
 gcloud iam workload-identity-pools providers create-oidc github-provider \
@@ -60,18 +55,14 @@ gcloud iam workload-identity-pools providers create-oidc github-provider \
   --issuer-uri="https://token.actions.githubusercontent.com"
 
 # Get the Workload Identity Provider ID
-gcloud iam workload-identity-pools providers describe github-provider \
-  --project="${PROJECT_ID}" \
-  --location="global" \
-  --workload-identity-pool="github-actions-pool" \
-  --format="value(name)"
+gcloud iam workload-identity-pools providers describe github-provider --location="global" --workload-identity-pool="github-actions-pool" --project="${PROJECT_ID}" --format="value(name)"
 
-# TODO:
 # Grant the cloud-run-deployer service account permission to be impersonated by the GitHub provider
-# gcloud iam service-accounts add-iam-policy-binding cloud-run-deployer@"${PROJECT_ID}".iam.gserviceaccount.com \
-#   --role="roles/iam.workloadIdentityUser" \
-#   --member="principalSet://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/${WIF_POOL_ID}/attribute.repository/${GITHUB_ORG}/${GITHUB_REPO}" \
-#   --project="${PROJECT_ID}"
+export PROJECT_NUMBER=$(gcloud projects describe "${PROJECT_ID}" --format="value(projectNumber)")
+gcloud iam service-accounts add-iam-policy-binding "cloud-run-deployer@${PROJECT_ID}.iam.gserviceaccount.com" \
+  --project="${PROJECT_ID}" \
+  --role="roles/iam.workloadIdentityUser" \
+  --member="principal://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/github-actions-pool/subject/repo:${GITHUB_ORG}/${GITHUB_REPO}:ref:refs/heads/main" # Or whichever branch you want to use
 
 # Create a Artifact Registry repository (this needs billing enabled)
 export GCP_REGION="europe-west9"
